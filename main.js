26e6f8eaf519ae04b9ca8b7f71989bac11f1f7e4
Anh Le <anhle_2021@depauw.edu>
2020-07-02 00:40:31 -0400

    Output the BMI Result as an Ion-Card

--- main.js
+++ main.js
@@ -2,12 +2,24 @@ var calButton = document.getElementById("calButton");
 var resetButton = document.getElementById("resetButton");
 var height = document.getElementById("height-input");
 var weight = document.getElementById("weight-input");
+var resultArea = document.getElementById("result");
 
 const bmi = () => {
-  var h = +height.value;
-  var w = +weight.value;
-  var result = w / (h * h);
-  console.log(result);
+  const h = +height.value;
+  const w = +weight.value;
+  const result = (w / (h * h)).toFixed(4);
+  if(isNaN(result)){
+    alert("Please enter numeric values");
+    return;
+  }
+  const resultElement = document.createElement('ion-card');
+  resultElement.innerHTML = `
+    <ion-card-content>
+      <h2>${result}</h2>
+    </ion-card-content>
+  `;
+  resultArea.innerHTML = "";
+  resultArea.appendChild(resultElement);
 };
 
 const resetInputs = () => {
